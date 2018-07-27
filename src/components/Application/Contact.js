4bd2599872ee055d19a977b9e434a94e7c70a59c
Robert Andrade <srandradejr@gmail.com>
2018-07-27 11:40:29 -0500

    finished application buttons and landing page

---
+++ src/components/Application/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                you are amazing
+            </div>
+         );
+    }
+}
+ 
+export default Contact;
---
+++ src/components/Application/Landing.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h2>Understanding the application process</h2>
+
+                <p>Explaining the process: 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur condimentum tellus ligula, sed feugiat est aliquam sit amet. Curabitur ultricies lacus eu nisi tincidunt, vel porta mauris mollis. Sed cursus ex quis rutrum varius. Aenean et mattis sem, id vestibulum erat. Fusce quis lectus vestibulum, finibus purus ac, iaculis nibh. Vivamus quis est id nulla ultricies varius. Maecenas feugiat est et sem interdum viverra. Duis mollis ligula ligula, quis cursus leo vulputate nec. Vivamus iaculis vitae lectus vitae feugiat. Integer vehicula ac ante eget euismod.
+                Etiam egestas congue tellus, nec dictum lectus gravida eu. Cras nec dui tellus. Praesent molestie quam quis leo laoreet tincidunt. Etiam et mauris mattis, elementum mauris at, iaculis nunc. Etiam eget arcu tincidunt, auctor augue at, elementum ipsum. Proin congue luctus convallis. Cras in nibh volutpat, efficitur urna vel, fringilla nulla. Nullam eget massa condimentum, rutrum nulla ac, fermentum orci. Nam auctor sem et tempus vehicula. Phasellus id ipsum in arcu aliquet blandit vel sit amet urna. Mauris auctor mi non felis scelerisque, vel accumsan ipsum venenatis. Integer congue leo ac neque suscipit, sit amet euismod mi imperdiet. Mauris lobortis arcu at quam faucibus mollis.
+                </p>
+                <ol>
+                    <li>Contact Information</li>
+                    <li>Demographics</li>
+                    <li>Income</li>
+                    <li>Expenses</li>
+                </ol>
+            </div>
+         );
+    }
+}
+ 
+export default Landing;
