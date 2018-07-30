b889f19b9ad747dffcd4be0244930557c110c8ca
Deric Aaron <aaro0034@umn.edu>
2018-07-30 13:32:48 -0500

    basic admin view

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -16,6 +16,27 @@ class AdminPage extends Component {
       <div>
         <h1>ADMIN PAGE</h1>
         {JSON.stringify(this.props.apps)}
+        <table>
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>ID</th>
+              <th>View</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {
+                    this.props.apps.map( thing =>    
+                      <tr>
+                      <td>{thing.first_name}</td>
+                      <td>{thing.first_name}</td>
+                      <td>{thing.first_name}</td>
+                      </tr>
+                    )
+                } */}
+          </tbody>
+        </table>
+
       </div>
     );
   }
