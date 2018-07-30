b1f2f6bc4cd87051f5b001f1e57aae85f9db7928
Dan Ross <dr.ross15@gmail.com>
2018-07-29 20:19:23 -0500

    basic GET all applications

--- server/routes/application.router.js
+++ server/routes/application.router.js
@@ -3,10 +3,21 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 /**
- * GET route template
+ * GET all information about every applicant
  */
 router.get('/', (req, res) => {
-    
+    let queryText = `SELECT * from form
+        JOIN contact on contact.form_id = form.id
+        JOIN demographics on demographics.form_id = form.id
+        JOIN expenses on expenses.form_id = form.id
+        JOIN income on income.form_id = form.id`;
+    pool.query(queryText)
+        .then(response => {
+            res.send(response.rows);
+        }).catch(err => {
+            console.log({err});
+            res.sendStatus(500);
+        })
 });
 
 /**
