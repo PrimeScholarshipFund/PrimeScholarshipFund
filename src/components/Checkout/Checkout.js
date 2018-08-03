3c8c1521bc1ec94ac581dd9e08f09305e6af1cce
Robert Andrade <srandradejr@gmail.com>
2018-08-02 20:57:04 -0500

    stripe working with card

---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
+
+import config from '../../config/config';
+
+const CURRENCY = 'EUR';
+
+
+const fromEuroToCent = amount => amount * 100;
+
+const successPayment = data => {
+    alert('Payment Successful');
+};
+
+const errorPayment = data => {
+    console.log('error', data);
+    
+    alert('Payment Error', data);
+};
+
+const onToken = (amount, description) => token =>
+axios.post(config.PAYMENT_SERVER_URL,
+    {
+        description,
+        source: token.id,
+        currency: CURRENCY,
+        amount: 1000
+    })
+    .then(successPayment)
+    .catch(errorPayment);
+    
+    const Checkout = ({ name, description, amount }) =>
+    <StripeCheckout
+    name='James dickenson'
+    description={description}
+    amount={1000}
+    token={onToken(amount, description)}
+    currency={CURRENCY}
+    stripeKey={config.STRIPE_PUBLISHABLE}
+    />
+    
+    export default Checkout;
