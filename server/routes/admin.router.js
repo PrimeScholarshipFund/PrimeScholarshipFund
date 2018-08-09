4eca209cbc2ac467472ae63a579c461f6e113f0a
Dan Ross <dr.ross15@gmail.com>
2018-08-09 13:20:33 -0500

    verify user is admin before getting all applicants

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -6,19 +6,26 @@ const router = express.Router();
  * GET all information about every applicant
  */
 router.get('/applicants', (req, res) => {
-    let queryText = `SELECT * from form
+    if(req.user.admin) {
+        let queryText = `SELECT * from form
         JOIN contact on contact.form_id = form.id
         JOIN demographics on demographics.form_id = form.id
         JOIN expenses on expenses.form_id = form.id
         JOIN income on income.form_id = form.id
         ORDER BY form.id`;
-    pool.query(queryText)
-        .then(response => {
-            res.send(response.rows);
-        }).catch(err => {
-            console.log({err});
-            res.sendStatus(500);
-        })
+
+        pool.query(queryText)
+            .then(response => {
+                res.send(response.rows);
+            }).catch(err => {
+                console.log({err});
+                res.sendStatus(500);
+            })
+    } else {
+        console.log(`you can't do that!!`);
+        res.sendStatus(500);
+    }
+    
 });
 
 router.put('/save', (req, res) => {
